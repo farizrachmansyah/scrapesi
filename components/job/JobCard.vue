bcf9c7dd9f7452815c3cf9ba9761d78e07d61e05
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2022-06-02 22:03:22 +0700

    create job card component

---
+++ components/job/JobCard.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="card">
+		<h3>ini job card</h3>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.card {
+	height: 300px;
+	border-radius: 8px;
+	background: $fourty;
+}
+</style>
