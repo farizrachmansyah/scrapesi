fde8aff087f483c1e8b365d5bbc060b2d1ac6eb8
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2022-06-19 19:46:07 +0700

    integrate card data

--- components/job/JobCard.vue
+++ components/job/JobCard.vue
@@ -1,13 +1,9 @@
 <template>
-	<a
-		href="https://farizrachmansyah.netlify.app"
-		target="_blank"
-		class="card p-12"
-	>
+	<a :href="cardData.url" target="_blank" class="card p-12">
 		<div class="mb-48">
-			<h1 class="job-name">Job Name</h1>
-			<h3 class="comp-name">Company Name</h3>
-			<p class="loc-name">Location</p>
+			<h1 class="job-name">{{ cardData.jobTitle }}</h1>
+			<h3 class="comp-name">{{ cardData.companyName }}</h3>
+			<p class="loc-name">{{ cardData.jobLocation }}</p>
 		</div>
 		<div class="text-light-grey text-right">
 			<span class="time-desc pv-4 ph-12 bg-fourty">Time description</span>
@@ -16,14 +12,23 @@
 </template>
 
 <script>
-export default {}
+export default {
+	props: {
+		cardData: {
+			type: Object,
+			default: () => {}
+		}
+	}
+}
 </script>
 
 <style lang="scss" scoped>
 .card {
 	box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.25);
-	display: inline-block;
+	display: flex;
+	flex-direction: column;
 	width: 100%;
+	height: 100%;
 	border-radius: 16px;
 	border: 1px solid $fourty;
 	transition: all 0.3s;
@@ -32,20 +37,27 @@ export default {}
 		box-shadow: none;
 	}
 
+	& > *:last-child {
+		margin-top: auto;
+	}
+
 	.job-name,
 	.comp-name,
 	.loc-name {
-		margin-bottom: 10px;
 		line-height: 1;
 	}
 
 	.job-name {
-		font-size: 28px;
+		font-size: 24px;
+		line-height: 1.2;
 	}
 	.comp-name,
 	.loc-name {
 		font-size: 16px;
 		font-weight: 400;
+	}
+	.comp-name {
+		margin-bottom: 4px;
 	}
 	.loc-name {
 		color: $grey;
