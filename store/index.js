f794d33c5ff5f4f7f87881ebdc547cf886dabf42
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2022-06-23 14:39:08 +0700

    setup nuxtserver init for do the scraping job

--- store/index.js
+++ store/index.js
@@ -7,3 +7,17 @@ export const mutations = {
 		state.searchKeyData = key
 	}
 }
+
+export const actions = {
+	async nuxtServerInit({ dispatch }, { route }) {
+		if (route.name === 'job-result') {
+			try {
+				const objParam = {
+					q: route.query.q,
+					loc: route.query.loc
+				}
+				await dispatch('job/getJobs', objParam)
+			} catch (error) {}
+		}
+	}
+}
