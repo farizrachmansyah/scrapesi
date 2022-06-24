55a881fcb22b621824694399766dcc6e0235ce0b
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2022-06-25 01:14:16 +0700

    remove history state and mutation to history store

--- store/index.js
+++ store/index.js
@@ -1,13 +1,3 @@
-export const state = () => ({
-	searchKeyData: []
-})
-
-export const mutations = {
-	SET_SEARCHKEY(state, key) {
-		state.searchKeyData = key
-	}
-}
-
 export const actions = {
 	async nuxtServerInit({ dispatch }, { route }) {
 		if (route.name === 'job-result') {
