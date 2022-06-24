29b3243409e4e7abf31c8ca3e6a19e0d47102483
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2022-06-25 01:13:05 +0700

    create history store

---
+++ store/history.js
@@ -0,0 +1,22 @@
+export const state = () => ({
+	searchKeyData: [],
+	recentKey: {}
+})
+
+export const getters = {
+	getHistory: state => () => {
+		return state.searchKeyData
+	},
+	getRecent: state => () => {
+		return state.recentKey
+	}
+}
+
+export const mutations = {
+	SET_HISTORY(state, key) {
+		state.searchKeyData = key
+	},
+	SET_RECENTKEY(state, key) {
+		state.recentKey = key
+	}
+}
