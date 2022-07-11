4eb1f1bc6767b9504aeefdbcc03a799736b49c58
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2022-07-12 01:58:43 +0700

    create new loading scrape popup handle

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,12 +9,34 @@
 		<div class="sticky-footer-container-item">
 			<Footer />
 		</div>
+
+		<!-- Popup -->
+		<div :class="{ scraping: isScraping }" class="popup-scraping">
+			<div class="popup-wrapper">
+				<div class="flex">
+					<div class="mr-16">
+						<i class="spinner"></i>
+					</div>
+					<div>
+						<h1 class="mb-10">Scraping data</h1>
+						<p class="mb-0">
+							Please wait, this process could take a few minutes.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'DefaultLayout',
+	data() {
+		return {
+			isScraping: false
+		}
+	},
 	head() {
 		return {
 			link: [
@@ -24,6 +46,11 @@ export default {
 				}
 			]
 		}
+	},
+	created() {
+		this.$nuxt.$on('startScrape', condition => {
+			this.isScraping = condition
+		})
 	},
 	mounted() {
 		this.$store.commit(
@@ -37,3 +64,40 @@ export default {
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+.popup-scraping {
+	position: absolute;
+	z-index: 4;
+	top: 0;
+	left: 0;
+	display: none;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+	background: rgba($black, 0.75);
+
+	&.scraping {
+		display: flex;
+	}
+
+	h1 {
+		font-size: 24px;
+	}
+}
+.popup-wrapper {
+	margin: 0 24px;
+	padding: 32px;
+	border-radius: 8px;
+	background: $white;
+}
+.spinner {
+	width: 24px;
+	height: 24px;
+	border-radius: 50%;
+	border: 3px solid rgba($black, 0.1);
+	border-top-color: lighten($primary, 5%);
+	animation: spinning 1s infinite both;
+}
+</style>
